8fb178c32dbf37a1cc0053b2d7bdec2019366bc6
FujinumaSatoru <gabriel.silva2016@gmail.com>
2022-10-04 11:52:24 -0300

    Fundo da tela login controlada pelo Model

---
+++ Lanchonete T92/LoginView.cs
@@ -0,0 +1,33 @@
+﻿// Importações de bibliotecas (classes)
+using System; // classe de sistema do Win
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text; // usar textos na tela
+using System.Threading.Tasks;
+using System.Windows.Forms; // usar componentes
+
+// namespace - agrupa as cliasses (pasta virtual)
+namespace Lanchonete_T92
+{
+    // classe partial - tela
+    public partial class LoginView : Form
+    {
+        // método construtor - é o único método executado automáticamente, sempre com o mesmo nome da classe.
+        public LoginView()
+        {
+            InitializeComponent();
+
+            // chamando o método que está no Model desta tela
+            // Instanciar a classe como um objetivo utilizável
+            LayoutModel login = new LayoutModel();
+
+            login.MontaLogin( this );
+
+        }
+
+
+    }
+}
---
+++ Lanchonete T92/Model/LayoutModel.cs
@@ -0,0 +1,68 @@
+﻿/*
+
+No model manipulamos armazenamos os dados principais da aplicação (cores, formatações, dados do BD).
+
+No Controller fazemos as chamadas que conectam a View e o Model.
+
+Na View temos os componentes de tela (textos, botões).
+
+    Tipos de dados para variáveis/atributos
+
+    int - numeros inteiros - 100
+    float - numeros decimais - 100.2f
+    bool - verdadeiro/falso - true/false
+    string - cadeias de texto - "texto 123"
+    char - letra única - 'c'
+
+    arrays permitem guardar mais de um valor na mesma variável/atributo
+        public int cores = 255;
+        public int[] cores = {255, 253, 251};
+        cores[0] 255 cores[1] 253
+
+    Tipos de Acesso
+        public - acesso liberado de qualquer classe do mesmo namespace.
+        private - acesso APENAS da classe onde foi criado.
+
+    Sixtaxe de atributo/variáveis
+        acesso tipoDeDado nomeAtributo = valor;
+
+    Sintaxe dos Métodos
+        acesso retorno NomeDoMétodo( parâmetros )
+        {
+            // Códigos a serem executados quando o método é chamado
+        }
+        
+        retorno ou é um tipo de dado ou a palavra void
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// importamos a classe de tela do Windows para manipular seus componentes
+using System.Windows.Forms;
+
+namespace Lanchonete_T92
+{
+    /// <summary>
+    /// Classe que controla a aparência do programa (tamanho, cor, etc)
+    /// </summary>
+    class LayoutModel
+    {
+        // Atributos
+        public int[] corFundoTelas = { 255, 60, 0 };
+        public int[] corTextoTelas = { 0, 0, 255 };
+
+        // Métodos
+
+        public void MontaLogin(Form tela)
+        {
+            // Mudar a cor de fundo da tela
+            tela.BackColor = Color.FromArgb( corFundoTelas[0],
+                corFundoTelas[1], corFundoTelas[2] );
+        }
+    }
+}
